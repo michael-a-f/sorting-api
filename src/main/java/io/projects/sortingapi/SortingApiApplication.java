dc7233656ee7501dc00a94419153a0f739160c80
Michael <michaelfarias@pop-os.localdomain>
2023-01-05 21:27:39 -0500

    initial commit

---
+++ src/main/java/io/projects/sortingapi/SortingApiApplication.java
@@ -0,0 +1,13 @@
+package io.projects.sortingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/projects/sortingapi/SortingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projects.sortingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
