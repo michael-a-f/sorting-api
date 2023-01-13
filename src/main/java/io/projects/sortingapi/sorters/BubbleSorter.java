53b9792140fd7ceb145f43280fb826517de151db
Michael Farias <mfarias496@gmail.com>
2023-01-12 21:50:31 -0500

    Add Bubble Sort

--- src/main/java/io/projects/sortingapi/sorters/BubbleSorter.java
+++ src/main/java/io/projects/sortingapi/sorters/BubbleSorter.java
@@ -3,6 +3,8 @@ package io.projects.sortingapi.sorters;
 import io.projects.sortingapi.Frame;
 import io.projects.sortingapi.Sorter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BubbleSorter implements Sorter {
@@ -20,6 +22,27 @@ public class BubbleSorter implements Sorter {
     }
 
     public List<Frame> generateSortFrames() {
+        if (list.size() == 0) {
+            return frames;
+        }
+        boolean isSorted = false;
+        int right = list.size() - 1;
+        while (!isSorted) {
+            int swaps = 0;
+            for (int i = 0; i < right; i++) {
+                frames.add(new Frame(new ArrayList<>(list), i));
+                if (list.get(i) > list.get(i + 1)) {
+                    Collections.swap(list, i, i+1);
+                    swaps++;
+                }
+            }
+            frames.add(new Frame(new ArrayList<>(list), right));
+            right--;
+            if (swaps == 0) {
+                isSorted = true;
+            }
+        }
+        frames.add(new Frame(new ArrayList<>(list), null));
         return this.frames;
     }
 }
